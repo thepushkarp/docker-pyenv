7f80f4e286d2468713f42f1d1d7a1c59278d87e1
Pushkar Patel <git@thepushkarp.com>
2024-04-01 03:31:46 +0530

    Update GitHub Action to use new versions and build on amd64 as well

--- update.sh
+++ update.sh
@@ -23,6 +23,54 @@ generated_warning() {
 EOF
 }
 
+# Get the machine hardware name
+architecture=$(uname -m)
+
+# Map the architecture to Docker platform codes
+case "$architecture" in
+    x86_64)
+        docker_platform="linux/amd64"
+        ;;
+    i386 | i486 | i586 | i686)
+        docker_platform="linux/386"
+        ;;
+    armv6l)
+        docker_platform="linux/arm/v6"
+        ;;
+    armv7l)
+        docker_platform="linux/arm/v7"
+        ;;
+    aarch64 | arm64 | armv8l)
+        docker_platform="linux/arm64"
+        ;;
+    ppc64)
+        docker_platform="linux/ppc64"
+        ;;
+    ppc64le)
+        docker_platform="linux/ppc64le"
+        ;;
+    s390x)
+        docker_platform="linux/s390x"
+        ;;
+    *)
+        echo "Unsupported architecture: $architecture"
+        exit 1
+        ;;
+esac
+
+versions=(
+    $(docker run --platform $docker_platform thepushkarp/pyenv sh -c " \
+        pyenv update >/dev/null 2>&1; \
+        for prefix in \$(pyenv install --list | \
+            grep -e '^  [[:digit:]]\(\.[[:digit:]]\+\)\+$' | \
+            cut -d. -f1,2 | \
+            sort -u -V) \
+        ; do \
+            pyenv latest --known \$prefix; \
+        done | sort -u -V"
+    )
+)
+
 versions=(
     $(docker run thepushkarp/pyenv sh -c " \
         pyenv update >/dev/null 2>&1; \
@@ -37,71 +85,6 @@ versions=(
 )
 
 declare -A blacklisted
-# https://github.com/vicamo/docker-pyenv/runs/4473301383
-blacklisted["buster-2.3.7"]=1
-# https://github.com/vicamo/docker-pyenv/runs/4473546041
-blacklisted["buster-2.4.6"]=1
-# https://github.com/vicamo/docker-pyenv/runs/4473706322
-blacklisted["buster-2.5.6"]=1
-blacklisted["buster-2.6.9"]=1
-blacklisted["buster-3.0.1"]=1
-blacklisted["buster-3.1.5"]=1
-blacklisted["buster-3.2.6"]=1
-blacklisted["buster-3.3.7"]=1
-blacklisted["buster-3.4.10"]=1
-# https://github.com/vicamo/docker-pyenv/runs/4473301216
-blacklisted["bullseye-2.5.6"]=1
-# https://github.com/vicamo/docker-pyenv/runs/4473545988
-blacklisted["bullseye-2.6.9"]=1
-# https://github.com/vicamo/docker-pyenv/runs/4473706117
-blacklisted["bullseye-3.0.1"]=1
-blacklisted["bullseye-3.1.5"]=1
-blacklisted["bullseye-3.2.6"]=1
-blacklisted["bullseye-3.3.7"]=1
-blacklisted["bullseye-3.4.10"]=1
-# https://github.com/vicamo/docker-pyenv/runs/4496645590
-blacklisted["bookworm-2.5.6"]=1
-blacklisted["bookworm-2.6.9"]=1
-blacklisted["bookworm-3.0.1"]=1
-blacklisted["bookworm-3.1.5"]=1
-blacklisted["bookworm-3.2.6"]=1
-blacklisted["bookworm-3.3.7"]=1
-blacklisted["bookworm-3.4.10"]=1
-blacklisted["bookworm-3.5.10"]=1
-# https://github.com/vicamo/docker-pyenv/runs/4493744141
-blacklisted["bionic-2.5.6"]=1
-blacklisted["bionic-2.6.9"]=1
-blacklisted["bionic-3.0.1"]=1
-blacklisted["bionic-3.1.5"]=1
-blacklisted["bionic-3.2.6"]=1
-blacklisted["bionic-3.3.7"]=1
-blacklisted["bionic-3.4.10"]=1
-blacklisted["focal-2.5.6"]=1
-blacklisted["focal-2.6.9"]=1
-blacklisted["focal-3.0.1"]=1
-blacklisted["focal-3.1.5"]=1
-blacklisted["focal-3.2.6"]=1
-blacklisted["focal-3.3.7"]=1
-blacklisted["focal-3.4.10"]=1
-blacklisted["jammy-2.5.6"]=1
-blacklisted["jammy-2.6.9"]=1
-blacklisted["jammy-3.0.1"]=1
-blacklisted["jammy-3.1.5"]=1
-blacklisted["jammy-3.2.6"]=1
-blacklisted["jammy-3.3.7"]=1
-blacklisted["jammy-3.4.10"]=1
-blacklisted["jammy-3.5.10"]=1
-blacklisted["alpine-2.1.3"]=1
-blacklisted["alpine-2.2.3"]=1
-blacklisted["alpine-2.3.7"]=1
-blacklisted["alpine-2.5.6"]=1
-blacklisted["alpine-2.6.9"]=1
-blacklisted["alpine-3.0.1"]=1
-blacklisted["alpine-3.1.5"]=1
-blacklisted["alpine-3.2.6"]=1
-blacklisted["alpine-3.3.7"]=1
-blacklisted["alpine-3.4.10"]=1
-blacklisted["alpine-3.5.10"]=1
 
 for dir in \
     alpine {buster,bullseye,bookworm,focal,jammy}{/slim,} \
